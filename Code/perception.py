353c4489ae0ee678529d9f11e2da94bbf21bc014
DedRec <89926533+DedRec@users.noreply.github.com>
2022-12-06 18:50:06 +0200

    commit 2

--- Code/perception.py
+++ Code/perception.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+
 # Identify pixels above the threshold
 # Threshold of RGB > 160 does a nice job of identifying ground pixels only
 def color_thresh(img, rgb_thresh=(160, 160, 160)):
@@ -18,13 +19,15 @@ def color_thresh(img, rgb_thresh=(160, 160, 160)):
     return color_select
 
 # Define a function to convert from image coords to rover coords
+
+
 def rover_coords(binary_img):
     # Identify nonzero pixels
     ypos, xpos = binary_img.nonzero()
-    # Calculate pixel positions with reference to the rover position being at the 
-    # center bottom of the image.  
-    x_pixel = -(ypos - binary_img.shape[0]).astype(np.float)
-    y_pixel = -(xpos - binary_img.shape[1]/2 ).astype(np.float)
+    # Calculate pixel positions with reference to the rover position being at the
+    # center bottom of the image.
+    x_pixel = -(ypos - binary_img.shape[0]).astype(np.float32)
+    y_pixel = -(xpos - binary_img.shape[1]/2).astype(np.float32)
     return x_pixel, y_pixel
 
 
@@ -38,15 +41,17 @@ def to_polar_coords(x_pixel, y_pixel):
     angles = np.arctan2(y_pixel, x_pixel)
     return dist, angles
 
+
 # Define a function to map rover space pixels to world space
 def rotate_pix(xpix, ypix, yaw):
     # Convert yaw to radians
     yaw_rad = yaw * np.pi / 180
     xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
-                            
+
     ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
-    # Return the result  
+    # Return the result
     return xpix_rotated, ypix_rotated
+
 
 def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
     # Apply a scaling and a translation
@@ -69,13 +74,24 @@ def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
     # Return the result
     return x_pix_world, y_pix_world
 
+
 # Define a function to perform a perspective transform
-def perspect_transform(img, src, dst):
-           
+def perspect_transform(img, src, dst, kernel_size=3):
+    img = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
     M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
-    
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))  # keep same size as input image
+
     return warped
+
+
+def find_rocks(img,Rock_thresh=(100,100,60)):
+    rockpix=((img[:,:,0]>Rock_thresh[0])\
+            & (img[:,:,1]>Rock_thresh[1])\
+            &(img[:,:,2]<Rock_thresh[2]))
+    color_select=np.zeros_like(img[:,:,0])
+    color_select[rockpix]=1
+
+    return color_select
 
 
 # Apply the above functions in succession and update the Rover state accordingly
@@ -102,8 +118,46 @@ def perception_step(Rover):
     # Update Rover pixel distances and angles
         # Rover.nav_dists = rover_centric_pixel_distances
         # Rover.nav_angles = rover_centric_angles
-    
- 
-    
-    
+    #  Objective 1
+    dst_size = 5
+    bottom_offset = 6
+    source = np.float32([[19, 140],
+                         [303, 140],
+                         [200, 95],
+                         [120, 95]
+                         ])
+    destination = np.float32([[Rover.img.shape[1] / 2 - dst_size, Rover.img.shape[0] - bottom_offset],
+                              [Rover.img.shape[1] / 2 + dst_size, Rover.img.shape[0] - bottom_offset],
+                              [Rover.img.shape[1] / 2 + dst_size, Rover.img.shape[0] - dst_size * 2 - bottom_offset],
+                              [Rover.img.shape[1] / 2 - dst_size, Rover.img.shape[0] - dst_size * 2 - bottom_offset]
+                              ])
+    #  Objective 2
+    warped = perspect_transform(Rover.img, source, destination, kernel_size=3)
+    #  Objective 3
+    obs_threshed = 1 - color_thresh(warped, (85, 85, 85))
+    rock_threshed = find_rocks(warped)  # find rock samples
+    path_threshed = color_thresh(warped)  # find only the navigatable path
+    #  Objective 4
+    Rover.vision_image[:, :, 0] = obs_threshed * 255
+    Rover.vision_image[:, :, 1] = rock_threshed * 255
+    Rover.vision_image[:, :, 2] = path_threshed * 255
+    #  Objective 5
+    xpix_obs, ypix_obs = rover_coords(Rover.vision_image[:, :, 0])
+    xpix_rock, ypix_rock = rover_coords(Rover.vision_image[:, :, 1])
+    xpix_path, ypix_path = rover_coords(Rover.vision_image[:, :, 2])
+    #  Objective 6
+    obs_x_world, obs_y_world = pix_to_world(xpix_obs, ypix_obs, Rover.pos[0], Rover.pos[1], Rover.yaw, Rover.worldmap.shape[0],
+                             2*dst_size)
+    rock_x_world, rock_y_world = pix_to_world(xpix_rock, ypix_rock, Rover.pos[0], Rover.pos[1], Rover.yaw, Rover.worldmap.shape[0],
+                              2 * dst_size)
+    navigable_x_world, navigable_y_world = pix_to_world(xpix_path, ypix_path,  Rover.pos[0], Rover.pos[1], Rover.yaw, Rover.worldmap.shape[0],
+                              2 * dst_size)
+    # Objective 7
+    Rover.worldmap[obs_y_world, obs_x_world, 0] += 1
+    Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
+    Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
+    # Objective 8
+    dist, angles = to_polar_coords(xpix_path, ypix_path)
+    Rover.nav_dists = dist
+    Rover.nav_angles = angles
     return Rover
