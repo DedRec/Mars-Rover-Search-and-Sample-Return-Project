96856cb8c76cc7b3e362a3e2f0a3a443062b4db2
Omar Ayman <75280540+oaayoub@users.noreply.github.com>
2022-12-31 22:27:57 +0200

    Update drive_rover.py

--- Code/drive_rover.py
+++ Code/drive_rover.py
@@ -138,7 +138,7 @@ def telemetry(sid, data):
             str_cnt = int((Rover.steer_count / Rover.max_steer_count) * 15)
             pos_cnt = int((Rover.pos_count / Rover.max_pos_count) * 15)
             print(
-                f"\r |steer count |{str_cnt * '█' + '-' * (15 - str_cnt)}|  |pos count| {pos_cnt * '▮' + '▯' * (15 - pos_cnt)} |rock_found| {int(Rover.gold_flag == True) * '⚫'} |mode| {Rover.mode} |P_Visited| {Rover.p_vis} |t_Visited| {Rover.t_vis} |total visits| {Rover.visited}",
+                f"\r |steer count |{str_cnt * '█' + '-' * (15 - str_cnt)}|  |pos count| {pos_cnt * '▮' + '▯' * (15 - pos_cnt)} |rock_found| {int(Rover.gold_flag == True) * '⚫'} |mode| {Rover.mode} |P_Visited| {Rover.p_vis} |t_Visited| {Rover.t_vis} |total visits| {len(Rover.t_vis)}",
                 end="\r", flush=True)
 
 
